3f69da368c07418c279d788582ef3ca988516a40
piyushmj12 <76661538+piyushmj12@users.noreply.github.com>
2023-02-07 18:50:14 +0530

    Add files via upload

---
+++ Day2assignment.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "instanceexample2" {
+  ami                     = "ami-01a4f99c4ac11b03c"
+  instance_type           = "t2.micro"
+   tags = {
+    Name = "Practiceinstance2"
+  }
+} 
